a7a35cce690a9f44e4e96129ed7d98f88449993d
fmoyarivas <fmoya@elconfidencial.com>
2019-01-16 11:24:05 +0100

    Initial commit

---
+++ SpinKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpinKit
+//
+//  Created by Fernando Moya de Rivas on 16/01/2019.
+//  Copyright © 2019 Fernando Moya de Rivas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
